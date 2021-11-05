4944332131636675efa26dfb74c7ea5eb0658ebb
Duong <Duongchauv@gmail.com>
2021-11-04 21:32:56 -0500

    Finished up delete routes and update routes for post and added better authentication that does not completely blocked unauthorized users from getting public posts

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,40 +6,62 @@ const jsonwebtoken = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const jwt = require('express-jwt');
 
+require('dotenv').config();
+
 exports.getHome = function(req,res,next){
     res.json({key:'test'});
 }
 
-exports.validId = function(req,res,next){
-    if(!(req.params.id.match(/^[0-9a-fA-F]{24}$/))){
-       res.status(404).json({errors: 'INVALID OBJECT ID'})
+exports.validId = [
+    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
+        .isString()
+        .trim()
+        .escape(),
+
+    param('commentId').isLength({min:24,max:24}).withMessage('Invalid ID')
+        .isString()
+        .trim()
+        .escape()
+        .optional(),
+
+    function(req,res,next){
+        let errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            res.status(404).json(errors);
+        }
+        else if(!(req.params.id.match(/^[0-9a-fA-F]{24}$/))){
+            res.status(404).json({errors: 'INVALID OBJECT ID'})
+        }
+        else if(req.params.commentId && !(req.params.commentId.match(/^[0-9a-fA-F]{24}$/))){
+            res.status(404).json({errors: 'INVALID OBJECT ID'})
+        }
+        else{
+            next();
+        }
     }
-    else if(req.params.commentId && !(req.params.commentId.match(/^[0-9a-fA-F]{24}$/))){
-        res.status(404).json({errors: 'INVALID OBJECT ID'})
-    }
-    else{
-        next();
-    }
-}
+]
 
 exports.login = [
     body('username').notEmpty().withMessage('Username cannot be empty')
+        .isString()
         .trim()
         .escape(),
     body('password').notEmpty().withMessage('Password cannot be empty')
+        .isString()
         .trim()
         .escape(),
     function(req,res,next){
         let errors = validationResult(req);
 
         if(!errors.isEmpty()){
-            res.json(errors)
+            res.json(errors) 
         }
         else{
             User.findOne({username:req.body.username})
                 .then(user=>{
                     if(!user){
-                        res.json({errors:'User not found'})
+                        res.json({errors:[{msg:'User not found'}]})
                     }
                     else{
                         bcrypt.compare(req.body.password,user.password,(err,response)=>{
@@ -47,13 +69,12 @@ exports.login = [
                                 res.json(err)
                             }
                             else if(response){
-                                //Secret should be env, test time
-                                let token = jsonwebtoken.sign({user}, 'testsecret')
-                                res.cookie('token', token, {httpOnly:true, maxAge:100000});
+                                let token = jsonwebtoken.sign({user}, process.env.TOKEN_SECRET)
+                                res.cookie('token', token, {httpOnly:true, maxAge:3600000});
                                 res.json({token})
                             }
                             else{
-                               res.json({errors:'Wrong password'}) 
+                               res.json({errors:[{msg:'Incorrect password'}]}) 
                             }
                         })
                     }
@@ -62,27 +83,46 @@ exports.login = [
         
     }
 ]
-exports.getPosts = function(req,res,next){  
-    Post.find({})
+exports.getPosts = [
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token,
+        credentialsRequired:false
+    }),  
+    function (req,res,next){
+        let allPost;
+
+        if(req.user){
+            allPost=Post.find({}).exec()
+        }
+        else{
+            allPost=Post.find({published:true}).exec()
+        }
+
+        allPost
         .then(posts=>{
             res.json(posts)
         })
         .catch(err=>{
             res.json(err)
         })
-}
+    }
+
+]
 
 exports.createPost = [
-    // Test middleware
     jwt({
-        secret:'testsecret',
+        secret:process.env.TOKEN_SECRET,
         algorithms: ['HS256'],
         getToken: req => req.cookies.token
     }),
     body('title').notEmpty().withMessage('Title Cannot be Empty')
+        .isString()
         .trim()
         .escape(),
     body('text').notEmpty().withMessage('Text cannot be empty')
+        .isString()
         .trim()
         .escape(),
     function(req,res,next){
@@ -97,7 +137,6 @@ exports.createPost = [
                 text:req.body.text,
 
             });
-
             post.save()
                 .then(savedPost => {
                     res.json(savedPost)
@@ -110,11 +149,81 @@ exports.createPost = [
 ]
 
 exports.getSinglePost = [
-    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
-        .trim()
-        .escape(),
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token,
+        credentialsRequired:false,
+    }),
 
     function(req,res,next){
+        Post.findById(req.params.id)
+            .then(post=>{
+                if(!post){
+                    res.status(404).json({errors:'Post does not exist'})
+                }
+                else if(post.published===false && !req.user){
+                    res.status(404).json({errors:'Post does not exist'})
+                }
+                else{
+                    res.json(post)
+                }
+            })
+            .catch(err=>{
+                res.json(err)
+            })
+    }
+]
+
+exports.deletePost = [
+
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token
+    }),
+    
+    function(req,res,next){
+        let deletePost = Post.findByIdAndDelete(req.params.id).exec();
+        let deleteComments = Comment.deleteMany({post:req.params.id}).exec();
+        
+        Promise.all([deletePost,deleteComments])
+        .then(([post,comments])=>{
+            if(!post){
+                res.json({errors:'NO POST FOUND'})
+            }
+            else{
+                res.json(post)
+            }
+        })
+        .catch(err=>{
+            res.json(err)
+        })
+    }
+]
+
+exports.updatePost = [
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token
+    }),
+
+    body('title')
+        .trim()
+        .escape()
+        .optional(),
+    body('text')
+        .trim()
+        .escape()
+        .optional(),
+    body('published')
+        .isBoolean()
+        .toBoolean()
+        .optional(),
+
+    function(req,res,next){
+        console.log(req.body.title, req.body.text);
         let errors = validationResult(req);
 
         if(!errors.isEmpty()){
@@ -124,122 +233,115 @@ exports.getSinglePost = [
             Post.findById(req.params.id)
                 .then(post=>{
                     if(!post){
-                        res.status(404).json({errors:'Post does not exist'})
+                        res.json({errors:'NO post found'})
                     }
                     else{
-                        res.json(post)
+                        if(req.body.title){
+                            post.title=req.body.title
+                        }
+                        if(req.body.text){
+                            post.text=req.body.text
+                        }
+                        if(req.body.published===true || req.body.published===false){
+                            post.published=req.body.published;
+                        }
+
+                        post.save()
+                            .then(response=>{
+                                res.json(response)
+                            })
+                            .catch(err=>{
+                                res.json(err)
+                            })
                     }
                 })
                 .catch(err=>{
-                    res.json(err)
+                    res.json(err);
                 })
-        }
-    }
-]
-
-exports.deletePost = [
-
-    jwt({
-        secret:'testsecret',
-        algorithms: ['HS256'],
-        getToken: req => req.cookies.token
-    }),
-    
-    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
-        .trim()
-        .escape(),
-    
-    function(req,res,next){
-        let errors = validationResult(req);
-
-        if(!errors.isEmpty()){
-            res.json(errors)
-        }
-        else{
-            let deletePost = Post.findByIdAndDelete(req.params.id).exec();
-            let deleteComments = Comment.deleteMany({post:req.params.id}).exec();
-          
-            Promise.all([deletePost,deleteComments])
-            .then(([post,comments])=>{
-                if(!post){
-                    res.json({errors:'NO POST FOUND'})
-                }
-                else{
-                    res.json(post)
-                }
-            })
-            .catch(err=>{
-              res.json(err)
-            })
         }
     }
 ]
 
 //Comments
 exports.getComments = [
-    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
-        .trim()
-        .escape()
-    ,
-    function(req,res,next){
-        let errors = validationResult(req);
 
-        if(!errors.isEmpty()){
-            res.json(errors)
-        }
-        else{
-            Comment.find({post:req.params.id})
-                .then(comments =>{
-                    res.json(comments);
-                })
-                .catch(err=>{
-                    res.json(err);
-                })
-        }
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token,
+        credentialsRequired:false,
+    }),
+
+    function(req,res,next){
+        Post.findById(req.params.id)
+            .then(post=>{
+                if(!post){
+                    res.status(404).json({errors:'Post does not exist'})
+                }
+                else if(post.published===false && !req.user){
+                    res.status(404).json({errors:'Post does not exist'})
+                }
+                else{
+                    Comment.find({post:req.params.id})
+                    .then(comments =>{
+                        res.json(comments);
+                    })
+                    .catch(err=>{
+                        res.json(err);
+                    })
+                }
+            })
+            .catch(err=>{
+                res.json(err)
+            })
     }
 ]
 
 exports.getComment = [
-    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
-    .trim()
-    .escape(),
-
-    param('commentId').isLength({min:24,max:24}).withMessage('Invalid ID')
-    .trim()
-    .escape(),
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token,
+        credentialsRequired:false,
+    }),
 
     function(req,res,next){
-        let errors = validationResult(req);
-
-        if(!errors.isEmpty()){
-            res.json(errors);
-        }
-        else{
-            Comment.findById(req.params.commentId)
-                .then(comment=>{
-                    if(!comment){
-                        res.json({errors:'COMMENT DOES NOT EXIST'});
-                    }
-                    else{
-                        res.json(comment)
-                    }
-                })
-                .catch(err=>{
-                    res.json(err);
-                })
-                
-        }
+        Post.findById(req.params.id)
+            .then(post=>{
+                if(!post){
+                    res.status(404).json({errors:'Post does not exist'})
+                }
+                else if(post.published===false && !req.user){
+                    res.status(404).json({errors:'Post does not exist'})
+                }
+                else{
+                    Comment.findOne({_id:req.params.commentId, post:req.params.id})
+                        .then(comment=>{
+                            if(!comment){
+                                res.json({errors:'COMMENT DOES NOT EXIST'});
+                            }
+                            else{
+                                res.json(comment)
+                            }
+                        })
+                        .catch(err=>{
+                            res.json(err);
+                        })  
+                }
+            })
+            .catch(err=>{
+                res.json(err)
+            })
     }
 ]
 
 exports.postComment = [
     body('name').notEmpty().withMessage('Name cannot be empty')
+        .isString()
         .trim()
         .escape(),
     body('text').notEmpty().withMessage('Text cannot be empty')
-        .trim()
-        .escape(),
-    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
+        .isString()
         .trim()
         .escape(),
     
@@ -279,38 +381,23 @@ exports.postComment = [
 
 exports.deleteComment = [
     jwt({
-        secret:'testsecret',
+        secret:process.env.TOKEN_SECRET,
         algorithms: ['HS256'],
         getToken: req => req.cookies.token
     }),
 
-    param('id').isLength({min:24,max:24}).withMessage('Invalid ID')
-        .trim()
-        .escape(),
-    
-    param('commentId').isLength({min:24,max:24}).withMessage('Invalid Comment ID')
-        .trim()
-        .escape(),
-
     function(req,res,next){
-        let errors = validationResult(req);
-
-        if(!errors.isEmpty()){
-            res.json(errors)
-        }
-        else{
-            Comment.findOneAndDelete({post:req.params.id, _id:req.params.commentId})
-                .then(comment=>{
-                    if(!comment){
-                        res.json({errors:'NO COMMENT OR POST FOUND'})
-                    }
-                    else{
-                        res.json(comment)
-                    }
-                })
-                .catch(err=>{
-                    res.json(err)
-                })
-        }
+        Comment.findOneAndDelete({post:req.params.id, _id:req.params.commentId})
+            .then(comment=>{
+                if(!comment){
+                    res.json({errors:'NO COMMENT OR POST FOUND'})
+                }
+                else{
+                    res.json(comment)
+                }
+            })
+            .catch(err=>{
+                res.json(err)
+            })
     }
 ]
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const apiController = require('../controllers/apiController');
-const Post = require('../models/post');
-const Comment = require('../models/comment');
-const {body,param,validationResult} = require('express-validator')
 
 router.get('/', apiController.getHome);
 router.post('/login', apiController.login)
@@ -14,14 +10,11 @@ router.post('/post', apiController.createPost);
 
 router.get('/post/:id', apiController.validId, apiController.getSinglePost);
 router.delete('/post/:id', apiController.validId, apiController.deletePost);
+router.put('/post/:id', apiController.validId, apiController.updatePost);
 
-//Comment Section
-//Only requires authentication on delete route
 router.get('/post/:id/comment', apiController.validId, apiController.getComments);
 router.post('/post/:id/comment', apiController.validId, apiController.postComment);
 router.get('/post/:id/comment/:commentId', apiController.validId, apiController.getComment);
 router.delete('/post/:id/comment/:commentId', apiController.validId, apiController.deleteComment);
-
-//MISSING UPDATES FOR POST AND LOGIN ROUTE
 
 module.exports = router;
